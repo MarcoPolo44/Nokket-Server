b9af6ad01c15232c7382f232a6a50710f927a785
Marco Politakis <marco.politakis@gmail.com>
2017-02-23 21:03:17 +1300

    Implemented basic nonce 'POST' controller

--- Nokket/Controllers/NonceController.cs
+++ Nokket/Controllers/NonceController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Braintree;
+using Microsoft.Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,11 +11,58 @@ namespace Nokket.Controllers
 {
     public class NonceController : ApiController
     {
-        // GET api/nonce/transaction
-        [HttpGet]
-        public string transaction()
+        public class FormData
         {
-            return "nonce";
+            public string nonce { get; set; }
+        }
+
+        public class JsonResult
+        {
+            public string message;
+        }
+
+        public IBraintreeConfiguration config = new BraintreeConfiguration();
+
+        // POST api/nonce/transaction
+        [HttpPost]
+        public IHttpActionResult transaction(FormData formData)
+        {
+            JsonResult jsonResult = new JsonResult();
+            var gateway = config.GetGateway();
+
+            string nonceFromTheClient = formData.nonce;
+
+            var request = new TransactionRequest
+            {
+                Amount = 10.00M,
+                PaymentMethodNonce = nonceFromTheClient,
+                Options = new TransactionOptionsRequest
+                {
+                    SubmitForSettlement = true
+                }
+            };
+
+            Result<Transaction> result = gateway.Transaction.Sale(request);
+            if (result.IsSuccess())
+            {
+                Transaction transaction = result.Target;
+                jsonResult.message = "created " + transaction.Id + " authorized";
+            }
+            else if (result.Transaction != null)
+            {
+                jsonResult.message = "";
+            }
+            else
+            {
+                string errorMessages = "";
+                foreach (ValidationError error in result.Errors.DeepAll())
+                {
+                    errorMessages += "Error: " + (int)error.Code + " - " + error.Message + "\n";
+                }
+                jsonResult.message = errorMessages;
+            }
+
+            return Ok(jsonResult);
         }
     }
 }
